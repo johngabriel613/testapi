9715adb2a13e76429e4e3cde473d381c4935bb1a
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2023-07-14 23:56:24 +0800

    .

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -3,12 +3,13 @@ import session from "express-session";
 const setSession = () => {
   return (
     session({
+      domain: '.netlify.app',
       secret: 'secretKey',
       resave: false,
       saveUninitialized: false,
       cookie:{
-        sameSite: "Lax",
-        secure:false,
+        sameSite: false,
+        secure:true,
         httpOnly:true,
         maxAge: 24 * 60 * 60 * 1000
       }
