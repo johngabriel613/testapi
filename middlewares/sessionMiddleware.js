d2acff0721c89cbc3852a63057ed609e0273aed0
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2023-07-15 10:08:44 +0800

    try

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -3,12 +3,12 @@ import session from "express-session";
 const setSession = () => {
   return (
     session({
-      domain: '.netlify.app',
+      domain: 'testbscs2b.netlify.app',
       secret: 'secretKey',
       resave: false,
       saveUninitialized: false,
       cookie:{
-        sameSite: false,
+        sameSite: "None",
         secure:true,
         httpOnly:true,
         maxAge: 24 * 60 * 60 * 1000
